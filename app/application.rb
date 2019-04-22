064d109f3a707aece84730657329a302323d2131
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-22 13:20:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,22 +1,19 @@
 class Application
-  
+ 
   def call(env)
-    resp = Rack::Response.new 
+    resp = Rack::Response.new
     
-    num_1 = Kernel.rand(1..20)
+   num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
-    
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
-    
-    if num_1 == num2 && num_2 == num_3
+ 
+    if num_1==num_2 && num_2==num_3
       resp.write "You Win"
-    else 
+    else
       resp.write "You Lose"
     end
+ 
     resp.finish
-  end 
-  
+  end
+ 
 end
